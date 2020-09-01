14cddcc1df85113304765ee8ce9a594ddd9b5b94
wuping24 <wuping24@gmail.com>
2020-09-01 18:10:44 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -32,6 +32,7 @@ rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/p
 
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy @rosy-主题
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge-动态登陆界面
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc  #argon-mc-主题
 git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
