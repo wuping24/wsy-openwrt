6dd86335d8f397bdc839d8d5f6d57c4b120842b6
wuping24 <wuping24@gmail.com>
2020-09-12 18:16:59 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -16,3 +16,4 @@
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git siropboy thttps://github.com/siropboy/mypackages' feeds.conf.default
