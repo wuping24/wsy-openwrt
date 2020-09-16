28f651377fadffc562ba3b51edb662035aebf696
wuping24 <wuping24@gmail.com>
2020-09-16 16:37:09 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -43,7 +43,7 @@ git clone https://github.com/fw876/helloworld.git package/lean/helloword
 #git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
-git clone https://github.com/lisaac/luci-app-dockerman/tree/master/applications/luci-app-dockerman.git package/lean/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman/.git package/lean/luci-app-dockerman
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
