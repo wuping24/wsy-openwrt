699f062203a3329d740c19a53e608a059d433d95
wuping24 <wuping24@gmail.com>
2020-09-17 15:54:47 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -41,7 +41,7 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
 git clone https://github.com/fw876/helloworld.git package/lean/helloword
 #git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+rm -rf ./package/lean/luci--app-jd-dailybonu && git clone -b https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonu  #京东签到
 git clone https://github.com/liuran001/openwrt-packages/tree/packages/luci-app-dockerman.git package/lean/luci-app-dockerman
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
